6f854e879bcaad0f2c35bf81fdbe2d81e84a8e10
Andreas <andreas.matejcek@gmail.com>
2022-05-09 11:48:53 +0200

    Typescript Update
    Typescript Update - Passwortlängenanzeige ist nun aktiv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
 
-import { helloWorld, Beispiel } from "./myModule";
+/* import { helloWorld, Beispiel } from "./myModule";
 import { alertMe } from "./myOtherModule";
 
 console.log(helloWorld);
@@ -22,3 +22,35 @@ function doSmth(e: UIEvent) {
   const val = e.target as HTMLInputElement;
   console.log(e, val.value);
 }
+*/
+
+const output = document.getElementById("outputFinal") as HTMLDivElement;
+const generateButton = document.getElementById("generatePassword") as HTMLButtonElement;
+generateButton.addEventListener("click", generatePassword);
+let lengthIndicator = document.getElementById("lengthIndicator") as HTMLSpanElement;
+
+
+window.onload = pwLengthDisplay;
+
+function pwLengthDisplay(){
+  let lengthField = document.getElementById("pwLength") as HTMLInputElement;
+  lengthField.addEventListener("change", function(){
+    lengthIndicator.innerHTML = this.value;
+  });
+}
+
+function generatePassword(){
+    let lengthField = document.getElementById("pwLength") as HTMLInputElement;
+    let uppercaseField = document.getElementById("uppercase") as HTMLInputElement;
+    let lowercaseField = document.getElementById("lowercase") as HTMLInputElement;
+    let symbolsField = document.getElementById("symbols") as HTMLInputElement;
+    let numbersField = document.getElementById("numbers") as HTMLInputElement;
+
+    let length = lengthField.value;
+    let uppercase = uppercaseField.checked;
+    let lowercase = lowercaseField.checked;
+    let symbols = symbolsField.checked;
+    let numbers = numbersField.checked;
+
+
+    }
